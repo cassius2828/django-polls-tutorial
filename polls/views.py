c3ae1e6be48e99547a77ae9daf78f72e4fac281a
Cassius <cassius.reynolds.dev@gmail.com>
2024-07-23 17:25:25 -0700

    wrote the url patterns for the detail, results, and vote view functions

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("hit")
+
+
+def detail(request, question_id):
+    return HttpResponse("You are looking the question %s." % question_id)
+
+
+def results(request, question_id):
+    response = f"You are looking at the results of question {question_id}."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on question {question_id}.")
